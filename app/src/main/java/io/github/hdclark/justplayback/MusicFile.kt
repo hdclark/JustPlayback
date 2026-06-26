065a42655cf9f82dc42f59bf2ceaa79af438a151
Hal Clark <hdclark@ualberta.ca>
2026-06-26 15:12:52 -0700

    Fix playlist handling and item text wrapping

--- app/src/main/java/io/github/hdclark/justplayback/MusicFile.kt
+++ app/src/main/java/io/github/hdclark/justplayback/MusicFile.kt
@@ -11,5 +11,5 @@ data class MusicFile(
     val path: String? = null
 ) : Serializable {
     val isPlaylist: Boolean
-        get() = name.endsWith(".m3u", ignoreCase = true)
+        get() = name.endsWith(".m3u", ignoreCase = true) || name.endsWith(".m3u8", ignoreCase = true)
 }
